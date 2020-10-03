9bad9dcb681fd5bd6d0ed158649b4a869c810467
Gamefist <ikheetdennis147@gmail.com>
2020-10-03 21:43:27 +0200

    Optimized imports

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,6 @@
-import discord
-from discord.ext import commands
 import random
+
+from discord.ext import commands
 
 
 class Fun(commands.Cog):
--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,5 @@
-import discord
 from discord.ext import commands
-import json
+
 from main import client as bot
 
 
